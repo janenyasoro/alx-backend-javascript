985f4ad8d1a8b9c4c6114a3f891ea26681b12363
janenyasoro <janenyasoro@gmail.com>
2024-10-15 23:36:45 +0300

    ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/5-main.js
@@ -0,0 +1,7 @@
+import createInt8TypedArray from "./5-typed_arrays.js";
+
+console.log(createInt8TypedArray(10, 2, 89));
+import createInt8TypedArray from "./5-typed_arrays.js";
+
+console.log(createInt8TypedArray(10, 2, 89));
+
